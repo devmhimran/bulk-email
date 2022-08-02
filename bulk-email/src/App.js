7a64b0b90af7a26a2dcfbf830d484e1089b7ecbc
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-08-02 14:31:01 +0600

    Added Required Files

---
+++ bulk-email/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+      <button class="btn btn-accent">Button</button>
+      <button class="btn btn-ghost">Button</button>
+      <button class="btn btn-link">Button</button>
+    </div>
+  );
+}
+
+export default App;
