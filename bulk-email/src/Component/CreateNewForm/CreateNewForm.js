c7e9c19970057019303b59b9ffe3fd1a9d94afc0
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-08-04 19:20:14 +0600

    Added Email Wizard

--- bulk-email/src/Component/CreateNewForm/CreateNewForm.js
+++ bulk-email/src/Component/CreateNewForm/CreateNewForm.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BsEnvelope } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const CreateNewForm = () => {
     return (
         <div className="container mx-auto ">
-            <h1 className='py-16'>Dashboard</h1>
+            <h1 className='py-16 text-3xl font-bold'>Dashboard</h1>
             <div className="create__email flex justify-center my-10">
                 <div className="create__email__card">
-                    <div class="card w-96 bg-base-100">
+                    <div class="card w-96 bg-base-100 drop-shadow-md">
                         <div class="card-body">
                             <div className='p-2 rounded-full text-center mx-auto text-2xl text-green-700 bg-green-400 my-6'>
                                 <BsEnvelope></BsEnvelope>
@@ -15,7 +16,7 @@ const CreateNewForm = () => {
                             <div className="content text-center">
                                 <h2 className='text-2xl font-medium mb-2.5'>Create New Email</h2>
                                 <p className='text-inherit'>Let's help you build a high-converting email using 1-1 personalized info</p>
-                                <button class="btn btn-wide btn-primary mt-6">Create New Email</button>
+                                <Link to='/dashboard/new' class="btn btn-wide btn-primary mt-6">Create New Email</Link>
                             </div>
                         </div>
                     </div>
--- bulk-email/src/Component/EmailWizard/EmailWizard.js
+++ bulk-email/src/Component/EmailWizard/EmailWizard.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { HiUserGroup } from 'react-icons/hi';
+import { BsEnvelopeOpen } from 'react-icons/bs';
+import { RiNumber1 } from 'react-icons/ri';
 
 const EmailWizard = () => {
     return (
-        <div>
-            email wizard
+        <div className='email__wizard flex items-center'>
+            <div className="container mx-auto">
+                <h1 className='py-16 text-3xl font-bold'>New Email Wizard</h1>
+                <div className="card bg-base-100 drop-shadow-md">
+                    <div className="card-body">
+                        <div className="wizard__info flex items-center">
+                            <div className="wizard__serial w-10">
+                                <span className='p-3 border rounded-full border-blue-700'>
+                                    1
+                                </span>
+                            </div>
+                            <div className="wizard__info__main ml-3">
+                                <h2 className='font-medium text-blue-700'>Who we are</h2>
+                                <p className='text-sm'>What are we pitching?</p>
+                            </div>
+                        </div>
+                        <div className="card border hover:border-cyan-500 my-1">
+                            <div className="card-body">
+                                <div className='flex item-center'>
+                                    <div className='inline-block align-middle text-3xl bg-teal-200 text-teal-700 p-3 rounded-full mr-4'><HiUserGroup></HiUserGroup></div>
+                                    <div className="email__wizard__info">
+                                        <p className='font-medium text-lg'>Find Tools</p>
+                                        <p className='text-inherit'>Find the right tools</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card border hover:border-cyan-500 my-1">
+                            <div className="card-body">
+                                <div className='flex item-center'>
+                                    <div className='inline-block align-middle text-3xl bg-blue-300 text-blue-700 p-3 rounded-full mr-4'><BsEnvelopeOpen></BsEnvelopeOpen></div>
+                                    <div className="email__wizard__info">
+                                        <p className='font-medium text-lg'>Personalizations only</p>
+                                        <p className='text-inherit'>Write just the first line personalizations, I don't need the whole email written</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
